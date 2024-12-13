d79227ab59857b5f8e1a27eca41fb8753c268ee4
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-12-13 16:42:58 +0100

    behavior

--- lib/ex_example.ex
+++ lib/ex_example.ex
@@ -3,17 +3,111 @@ defmodule ExExample do
   Documentation for `ExExample`.
   """
   alias ExExample.Analyze
+  alias ExExample.Cache
   alias ExExample.Executor
+
+  ############################################################
+  #                       Types                              #
+  ############################################################
+
+  @typedoc """
+  A dependency is a function that will be called by an example.
+  The format of a dependency is `{{module, function}, arity}`
+  """
+  @type dependency :: {{atom(), atom()}, non_neg_integer()}
+
+  @typedoc """
+  """
+  @type example :: {atom(), list(dependency)}
+
+  ############################################################
+  #                       Helpers                            #
+  ############################################################
+
+  @doc """
+  I return the hidden name of an example.
+  The hidden name is the example body without modification.
+  """
+  @spec hidden_name({atom(), atom()}) :: {atom(), atom()}
+  def hidden_name({module, func}) do
+    {module, String.to_atom("__#{func}__")}
+  end
+
+  @doc """
+  I determine if a module/function pair is an example or not.
+
+  A function is an example if it is defined in a module that has the `__examples__/0` function
+  implemented, and when the `__examples__()` output lists that function name as being an example.
+  """
+  @spec example?(dependency()) :: boolean()
+  def example?({{module, func}, _arity}) do
+    example_module?(module) and Keyword.has_key?(module.__examples__(), func)
+  end
+
+  @doc """
+  I return true if the given module contains examples.
+  """
+  @spec example_module?(atom()) :: boolean
+  def example_module?(module) do
+    {:__examples__, 0} in module.__info__(:functions)
+  end
+
+  @doc """
+  I return a list of all dependencies for this example.
+  Note: this does includes other called modules too (e.g., Enum).
+  """
+  @spec all_dependencies({atom(), atom()}) :: [dependency()]
+  def all_dependencies({module, func}) do
+    module.__examples__()
+    |> Keyword.get(func, [])
+  end
+
+  @doc """
+  I return a list of example dependencies for this example.
+  Note: this does not include other called modules.
+  """
+  @spec example_dependencies({atom(), atom()}) :: [dependency()]
+  def example_dependencies({module, func}) do
+    all_dependencies({module, func})
+    |> Enum.filter(&example?/1)
+  end
+
+  @doc """
+  I return a list of examples in the order they should be
+  executed in.
+
+  I do this by topologically sorting their execution order.
+  """
+  @spec execution_order(atom()) :: [{atom(), atom()}]
+  def execution_order(module) do
+    module.__examples__()
+    |> Enum.reduce(Graph.new(), fn
+      {function, []}, g ->
+        Graph.add_vertex(g, {__MODULE__, function})
+
+      {function, dependencies}, g ->
+        dependencies
+        # filter out all non-example dependencies
+        |> Enum.filter(&example?/1)
+        |> Enum.reduce(g, fn {{module, func}, _arity}, g ->
+          Graph.add_edge(g, {module, func}, {module, function})
+        end)
+    end)
+    |> Graph.topsort()
+  end
+
+  ############################################################
+  #                       Macros                             #
+  ############################################################
 
   defmacro __using__(_options) do
     quote do
       import unquote(__MODULE__)
 
+      @behaviour ExExample.Behaviour
+
       # module attribute that holds all the examples
-      Module.register_attribute(__MODULE__, :example_dependencies, accumulate: true)
       Module.register_attribute(__MODULE__, :examples, accumulate: true)
-      Module.register_attribute(__MODULE__, :copies, accumulate: true)
-      Module.register_attribute(__MODULE__, :copy, accumulate: false)
 
       @before_compile unquote(__MODULE__)
     end
@@ -21,61 +115,8 @@ defmodule ExExample do
 
   defmacro __before_compile__(_env) do
     quote do
-      @doc """
-      I return a list of all the dependencies for a given example,
-      or the list of all dependencies if no argument is given.
-      """
-      def __example_dependencies__, do: @example_dependencies
-
-      def __example_dependencies__(dependee) do
-        @example_dependencies
-        |> Enum.find({nil, []}, fn {name, _} -> name == dependee end)
-        |> elem(1)
-      end
-
-      @doc """
-      I reutrn all the examples in this module.
-      """
-      def __examples__ do
-        @examples
-      end
-
-      @doc """
-      I run all the examples in this module.
-      """
-      def __run_examples__ do
-        __sorted__()
-        |> Enum.each(fn {module, name} ->
-          apply(module, name, [])
-        end)
-      end
-
-      @doc """
-      I return a topologically sorted list of examples.
-      This list is the order in which the examples should be run.
-      """
-      @spec __sorted__() :: list({atom(), atom()})
-      def __sorted__ do
-        __example_dependencies__()
-        |> Enum.reduce(Graph.new(), fn
-          {example, []}, g ->
-            Graph.add_vertex(g, {__MODULE__, example})
-
-          {example, dependencies}, g ->
-            dependencies
-            # filter out all non-example dependencies
-            |> Enum.filter(&Executor.example?/1)
-            |> Enum.reduce(g, fn {{module, func}, _arity}, g ->
-              Graph.add_edge(g, {module, func}, {__MODULE__, example})
-            end)
-        end)
-        |> Graph.topsort()
-      end
-
-      def __example_copy__(example_name) do
-        @copies
-        |> Keyword.get(example_name, nil)
-      end
+      @spec __examples__ :: [ExExample.example()]
+      def __examples__, do: @examples
     end
   end
 
@@ -91,24 +132,22 @@ defmodule ExExample do
     hidden_example_name = String.to_atom("__#{example_name}__")
 
     quote do
-      # fetch the attribute value, and then clear it for the next examples.
-      example_copy_tag = Module.get_attribute(unquote(__CALLER__.module), :copy)
-      Module.delete_attribute(unquote(__CALLER__.module), :copy)
-
       def unquote({hidden_example_name, context, args}) do
         unquote(body)
       end
 
-      @copies {unquote(example_name), {unquote(__CALLER__.module), example_copy_tag}}
-      @example_dependencies {unquote(example_name), unquote(called_functions)}
-      @examples unquote(example_name)
+      @examples {unquote(example_name), unquote(called_functions)}
       def unquote(name) do
-        example_dependencies = __example_dependencies__(unquote(example_name))
-        example_copy = __example_copy__(unquote(example_name))
+        case Executor.attempt_example({__MODULE__, unquote(example_name)}, []) do
+          %{result: %Cache.Result{success: :success} = result} ->
+            result.result
 
-        Executor.maybe_run_example(__MODULE__, unquote(example_name), example_dependencies,
-          copy: example_copy
-        )
+          %{result: %Cache.Result{success: :failed} = result} ->
+            raise result.result
+
+          %{result: %Cache.Result{success: :skipped} = result} ->
+            :skipped
+        end
       end
     end
   end
